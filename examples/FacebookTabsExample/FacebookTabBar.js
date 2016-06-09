d42dee738079dc61a246f968b1e5fb5bdfcc8c9e
Theo Li <bbtfrr@gmail.com>
2016-06-10 00:44:19 +0800

    fix FacebookTabBar icon color flashing issue

--- examples/FacebookTabsExample/FacebookTabBar.js
+++ examples/FacebookTabsExample/FacebookTabBar.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Animated,
 } from 'react-native';
+
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const FacebookTabBar = React.createClass({
@@ -18,13 +19,13 @@ const FacebookTabBar = React.createClass({
   },
 
   componentDidMount() {
-    this.setAnimationValue({ value: this.props.activeTab, });
     this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
   },
 
   setAnimationValue({ value, }) {
     this.tabIcons.forEach((icon, i) => {
-      const progress = (value - i >= 0 && value - i <= 1) ? value - i : 1;
+      const progress = (value >= i && value < i + 1) ? value - i :
+        (value >= i - 1 && value < i) ? i - value : 1;
       icon.setNativeProps({
         style: {
           color: this.iconColor(progress),
@@ -41,27 +42,38 @@ const FacebookTabBar = React.createClass({
     return `rgb(${red}, ${green}, ${blue})`;
   },
 
+  renderTabIcons() {
+    return this.props.tabs.map((tab, i) => {
+      return (
+        <TouchableOpacity
+          key={tab}
+          onPress={() => this.props.goToPage(i)}
+          style={styles.tab}
+          >
+          <Icon
+            name={tab}
+            size={30}
+            ref={(icon) => { this.tabIcons[i] = icon; }}
+            />
+        </TouchableOpacity>
+      );
+    });
+  },
+
   render() {
     const tabWidth = this.props.containerWidth / this.props.tabs.length;
     const left = this.props.scrollValue.interpolate({
       inputRange: [0, 1, ], outputRange: [0, tabWidth, ],
     });
 
-    return <View>
-      <View style={[styles.tabs, this.props.style, ]}>
-        {this.props.tabs.map((tab, i) => {
-          return <TouchableOpacity key={tab} onPress={() => this.props.goToPage(i)} style={styles.tab}>
-            <Icon
-              name={tab}
-              size={30}
-              color={this.props.activeTab == i ? 'rgb(59,89,152)' : 'rgb(204,204,204)'}
-              ref={(icon) => { this.tabIcons[i] = icon; }}
-            />
-          </TouchableOpacity>;
-        })}
+    return (
+      <View>
+        <View style={[styles.tabs, this.props.style, ]}>
+          {this.renderTabIcons()}
+        </View>
+        <Animated.View style={[styles.tabUnderlineStyle, { width: tabWidth }, { left, }, ]} />
       </View>
-      <Animated.View style={[styles.tabUnderlineStyle, { width: tabWidth }, { left, }, ]} />
-    </View>;
+    );
   },
 });
 
